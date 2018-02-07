9c4d5256b0073f3145c1f55099efe9806b3a6327
kjbetts <kjbetts@github.com>
2018-02-06 21:58:36 -0500

    Master

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-    var table;
+  var table;
   $(".table").on("click", function() {
-     table= $(event.target);
+    table = $(event.target);
     $(".tn").append('<p>Table Number: </p>' + table.text());
     $(".reservations").fadeIn('slow');
 
   })
   $("#Save").on("click", function() {
     table.addClass('selected');
-  $(".reservations").fadeOut('slow');
-  $('.tn').empty();
+    $(".reservations").fadeOut('slow');
+    $('.tn').empty();
 
   })
   //when user clicks save button, console.log the values of the the inputs
-  $("#Save").on("click", function(){
-    var name= $("#Name").val();
-    var phone= $("#Phone").val();
-    var number= $("#NumberofPeople").val();
+  $("#Save").on("click", function() {
+    var name = $("#Name").val();
+    var phone = $("#Phone").val();
+    var number = $("#NumberofPeople").val();
     //append a div to the table
-    table.append("<div class='hover-div'><div>Guest Name: " + name + "</div><div>Phone Number:"  + phone+ "</div><div>Size of Party:" + number + "</div></div>");
+    table.append("<div class='hover-div'><div>Guest Name: " + name + "</div><div>Phone Number:" + phone + "</div><div>Size of Party:" + number + "</div></div>");
   })
 
   //when click on x hide the form
-$("#xbutton").on("click", function(){
-  $(".reservations").fadeOut("slow");
-})
+  $("#xbutton").on("click", function() {
+    $(".reservations").fadeOut("slow");
+  })
 
 
 
